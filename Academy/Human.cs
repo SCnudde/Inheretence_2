2b19ad142cb1d248fda4d4486b400b12234863d0
SCnudde <125920286+SCnudde@users.noreply.github.com>
2025-11-05 15:38:32 +0300

    Add project files.

---
+++ Academy/Human.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    internal class Human
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+
+        public Human(string lastName, string firstName, int age)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Age = age;
+            Console.WriteLine($"HConstructor:\t {GetHashCode()}");
+        }
+
+        public Human (Human other)// Конструктор копирования
+        {
+            LastName = other.LastName;
+            FirstName = other.FirstName;
+            Age = other.Age;
+            Console.WriteLine($"CopyCOnstructor: \t {GetHashCode() }");
+        }
+
+        ~Human()
+        {
+            Console.WriteLine($"HDestructor:\t {GetHashCode()}");
+        }
+
+        public virtual void Info()
+        {
+            Console.WriteLine($"{LastName} {FirstName} {Age}");
+        }
+
+        public override string ToString()
+        {
+                return //Split ('.') разделяет "Academy.Type" по точке на массив строк, и из этого массива
+                      //мы берем последний элемент
+                $"{ base.ToString().Split('.').Last()}:".PadRight(10) +
+                $" {LastName.PadRight(10)} {FirstName.PadRight(10)} {Age.ToString().PadRight(5)}";
+        // PadRight() - выравнивает строку по левому борту. от Padding - выравнивание
+        
+        }
+    }
+}
---
+++ Academy/Program.cs
@@ -0,0 +1,79 @@
+﻿//#define INHERITANCE_1
+#define INHERITANCE_2
+
+using Academy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACADEMY
+{
+    internal class Program
+    {
+        static readonly string delimiter = "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n";
+
+
+        static void Main(string[] args)
+        {
+#if INHERITANCE_1
+            Human human = new Human("Montana", "Antonio", 25);
+            human.Info();
+
+            Console.WriteLine(delimiter);
+
+            Student student = new Student("Pinkman","Jessie", 22, "Chemistry", "WW_220",90,95);
+            student.Info();
+            Console.WriteLine(delimiter);
+
+
+            Teacher teacher = new Teacher("White", "Walter", 50, "Chemistry", 25);
+            teacher.Info();
+            Console.WriteLine(delimiter);
+#endif
+#if INHERITANCE_2
+
+            Human human = new Human("Pinkman", "Jessie", 22);
+            human.Info();
+            Console.WriteLine(delimiter);
+
+
+            Student student = new Student(human, "Chemistry", "ww_220", 90, 95);
+            student.Info();
+            Console.WriteLine(delimiter);
+
+            Teacher teacheer = new Teacher(new Human("White", "Waiter", 50), "Chemistry", 25);
+            teacheer.Info();
+            Console.WriteLine(delimiter);
+
+            Human h_hank = new Human("Schreder", "Hank", 40);
+            Student s_hank = new Student(h_hank, "Criminalistics", "OBN", 50, 60);
+            Graduate graduate = new Graduate(s_hank, "How to catch Heisenberg");
+            graduate.Info();
+            Console.WriteLine(delimiter);
+
+#endif
+            //Base-class pointers: Generalisation
+            //(Upcast - приведение дочернего объекта к базовуму типу)
+            Human[] group =
+            {
+                new Student ("Pinkman", "Jessie", 22, "Chemistry", "ww_220", 90, 95),
+                new Teacher ("White", "Waiter", 50,"Chemistry", 25),
+                new Graduate ("Schreder", "Hank", 40, "Criminalistics", "OBN", 50, 60, "How to catch Heisenberg"),
+                new Student ("Vercetty", "Tommy", 30,"Theft", "Vice", 98, 99),
+                new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
+            };
+            Console.WriteLine(delimiter);
+
+            //Specialisation: виртуальный метод  virtual - override
+
+            for (int i = 0; i < group.Length; i++)
+            {
+                //group[i].Info();
+                Console.WriteLine(group[i].ToString());
+                Console.WriteLine(delimiter);
+            }
+        }
+    }
+}
